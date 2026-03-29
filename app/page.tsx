d27548d8ace5c350f5cf7632d5030c3234a7e4ad
RobynAwesome <rkholofelo@gmail.com>
2026-03-30 00:45:37 +0200

    modified:   app/page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,5 @@
-import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 import HomeFeed from "@/components/HomeFeed";
 
 export default function Home() {
-  return (
-    <>
-      <SignedIn>
-        <HomeFeed />
-      </SignedIn>
-      <SignedOut>
-        <RedirectToSignIn />
-      </SignedOut>
-    </>
-  );
+  return <HomeFeed />;
 }
